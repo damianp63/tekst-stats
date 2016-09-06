2902c26edcf6a3eae19f1c1714b0a54e21c76979
damianp63 <damianpodrazka@wp.pl>
2016-09-06 20:49:21 +0200

    użycie join i reduce

--- tekst_stats.rb
+++ tekst_stats.rb
@@ -9,17 +9,16 @@ class TextStats
     text.scan(/\w+/)
   end
 
-  def stats
+  def count
     tokens.each_with_object(Hash.new(0)) { |item, memo| memo[item] += 1 }
   end
 
   def sorted_stats
-    stats.sort_by { |token, count| [-count, token] }
+    count.sort_by { |token, count| [-count, token] }
   end
 
   def show
-    stats = ""
-    sorted_stats.each { |token, count| stats += "[#{token}:#{count}] " }
+    stats = sorted_stats.reduce([]) { |memo, item| memo << item.join(':') }.join('] [') + ']'
     token = text.gsub(/\w+/) { |word| "[#{word}]" }
     "#{self.class} text: #{text.inspect}, tokenized: #{token.inspect}, stats: #{stats.inspect}"
   end
