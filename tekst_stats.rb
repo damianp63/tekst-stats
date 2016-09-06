28d3661c0bd5e9e6fda629ef7ac37d99a2ff2903
damianp63 <damianpodrazka@wp.pl>
2016-09-06 16:25:56 +0200

    chyba koniec

--- tekst_stats.rb
+++ tekst_stats.rb
@@ -13,15 +13,19 @@ class TextStats
     tokens.each_with_object(Hash.new(0)) { |item, memo| memo[item] += 1 }
   end
 
-  def sort
+  def sorted_stats
     hash = stats
     hash = hash.sort { |previous, after| after <=> previous }
-    hash.sort_by { |_key, value| value }.reverse.to_h
+    hash.sort_by { |_key, value| value }.reverse
   end
 
   def show
-    [text, tokens, sort]
+    statistics = ""
+    token = text
+    sorted_stats.each { |item| statistics += "[#{item[0]}:#{item[1]}] " }
+    token = token.gsub(/\w+/) { |word| "[" + word + "]" }
+    "#{self.class} text: #{text}, tokenized: #{token}, stats: #{statistics}"
   end
 end
 
-puts TextStats.new(STDIN.read).show.inspect
+print TextStats.new(STDIN.read).show.inspect
